93f2fa5ee646cf09961c2b3351be7e715f2cd441
Evan Casey <evan.casey92@gmail.com>
2014-03-09 17:44:08 -0700

    more docs and check for duplicate links

--- README.md
+++ README.md
@@ -1,6 +1,25 @@
 #Fillinbox
 
+###Getting Started
+
+Install node dependencies:
+```bash
+$ npm install
+$ npm install nodemon
+```
+
+Start the web server:
+```bash
+$ nodemon app.js
+```
+
 ###Links
+
+Install python dependencies:
+```bash
+$ pip install -r utils/requirements.txt
+```
+
 Run:
 ```bash
 $ python utils/links.py
--- utils/links.py
+++ utils/links.py
@@ -3,11 +3,18 @@ from google import search
 
 connect('fillinbox', host='mongodb://fillinbox:westside@ds033579.mongolab.com:33579/fillinbox')
 
+# mongo schema
 class Link(DynamicDocument):
+    query = StringField(required=True)
     url = StringField(required=True)
 
-for url in search('newsletter signup',stop=10000):
-    new_link = Link()
-    new_link.url = url
-    new_link.save()
-    print url
+QUERY = 'newsletter signup'
+
+for url in search(QUERY,stop=10000):
+    # TODO: pause between each page to avoid 503 errors (Captcha)
+    if not list(Link.objects(url=url)):
+        new_link = Link()
+        new_link.url = url
+        new_link.query = QUERY
+        new_link.save()
+        print url
