afdc169bf3e732ecf3366059f10a35fce93f07b9
Evan Casey <evan.casey92@gmail.com>
2014-04-01 00:14:52 -0700

    form and modal

--- app/views.py
+++ app/views.py
@@ -1,6 +1,14 @@
 from flask import Flask, request, redirect, render_template, session, url_for, jsonify
 from app import app
+import pdb
 
 @app.route('/',methods=['GET'])
 def index():
     return render_template('index.html')
+
+@app.route('/create',methods=['POST'])
+def create():
+    if request.method == "POST":
+        data = request.form
+        print data
+        return jsonify(data)
